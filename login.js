b1ec74a01f101fe5946112c24f679ee04a6dcda7
MiaDRyu <miajoha.rhz@gmail.com>
2026-06-27 15:26:55 -0600

    Login Funcionando - Mia

--- login.js
+++ login.js
@@ -4,20 +4,29 @@ document.getElementById("loginForm").addEventListener("submit", async function(e
     const correo = document.querySelector('input[name="correo"]').value;
     const password = document.querySelector('input[name="password"]').value;
 
-    const res = await fetch("http://localhost:3000/api/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ correo, password })
-    });
+    try {
+        const res = await fetch("http://localhost:3000/api/usuarios/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ email: correo, password: password }) 
+        });
 
-    const data = await res.json();
+        const data = await res.json();
 
-    if (data.success) {
-        alert("Login correcto");
-        window.location.href = "dashboard.html";
-    } else {
-        alert("Usuario o contraseña incorrectos");
+        if (res.ok && data.token) {
+            
+            localStorage.setItem('nexura_token', data.token);
+            localStorage.setItem('nexura_usuario', JSON.stringify(data.usuario));
+
+            alert(`¡Bienvenido(a) ${data.usuario.nombre}!`);
+            window.location.href = "panel.html";
+        } else {
+            alert(data.Mensaje || "Error al iniciar sesión");
+        }
+    } catch (error) {
+        console.error("Error de conexión:", error);
+        alert("Error al conectar con el servidor.");
     }
-}); 
+});
